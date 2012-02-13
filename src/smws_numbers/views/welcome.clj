2d357035c338d487ef4641979596709fe0059e0f
Adrian Mowat <adrian.mowat@gmail.com>
2012-02-13 13:45:31 +0000

    Moved the search results onto the same page as the search box

--- src/smws_numbers/views/welcome.clj
+++ src/smws_numbers/views/welcome.clj
@@ -7,26 +7,29 @@
         [hiccup.form-helpers]
         [smws_numbers.models.distillery_codes :as distillery-codes]))
 
+(defn lookup [search-string]
+  (find distillery-codes/codes search-string))
+
+(defpartial search-result [{:keys [bottle-number distillery-name]}]
+  [:p (str bottle-number ": " distillery-name)])
+
 (defpage "/" [] (resp/redirect "/distilleries"))
 
-(defpage "/distilleries" []
+(defpage "/distilleries" {:as params}
+  (let [bottle-number (first params) distillery-name (second params)]
   (common/layout
     [:h1 "SMWS Bottling Search"]
     (form-to [:post "/distilleries/search"]
-      (text-field "search")
-      [:input {:id "submit-button" :type "submit" :value "search"}])))
+      [:input {:id "search" :name "search" :type "text" :value bottle-number}]
+      [:input {:id "submit-button" :type "submit" :value "search"}])
+    (search-result {:bottle-number bottle-number :distillery-name distillery-name}))))
 
 (defpage [:post "/distilleries/search"] {bottle-number :search}
-  (let [distillery (find distillery-codes/codes bottle-number)]
+  (let [distillery (lookup bottle-number)]
+    (println (str "Search for: " bottle-number))
     (if (nil? distillery)
       (render "/distilleries/notfound" bottle-number)
-      (render "/distilleries/searchresults" distillery))))
-
-(defpage "/distilleries/searchresults" [bottle-number distillery-name]
-  (common/layout
-    [:h2 "Search results..."
-      [:p (str bottle-number ": " distillery-name)]
-      (link-to "/distilleries" "Search Again")]))
+      (render "/distilleries" distillery))))
 
 (defpage "/distilleries/notfound" {:as bottle-number}
   (common/layout
